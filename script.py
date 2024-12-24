13741069ee80aeecd47788c6db68e57a28eed364
Mikael Bååth <mikaelbaath@gmail.com>
2024-12-24 10:49:58 +0100

    support for multiple cameras

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 from discord.ext import tasks
 from cv2 import VideoCapture, imwrite, imread
@@ -6,31 +8,36 @@ from cv2 import VideoCapture, imwrite, imread
 class Bot(discord.Client):
     @tasks.loop(seconds=1)
     async def monitor(self):
-        if await self.movement('last_frame.png'):
+        if await self.movement():
             print('Movement detected!')
-            await self.save_latest_frame_from_webcam('last_frame.png')
-            await self.send_image_to_discord('last_frame.png')
+            await self.save_latest_frames_from_webcam()
+            await self.send_images_to_discord()
         else:
             print('No movement detected')
-        await self.save_latest_frame_from_webcam('last_frame.png')
+        await self.save_latest_frames_from_webcam()
 
-    async def save_latest_frame_from_webcam(self, output_file):
-        ret, frame = self.cap.read()
-        if not ret:
-            return False
-        imwrite(output_file, frame)
-        return True
+    async def save_latest_frames_from_webcam(self):
+        for i, cap in enumerate(self.caps):
+            output_file = f'last_frame_{i}.png'
+            ret, frame = cap.read()
+            if not ret:
+                continue
+            imwrite(output_file, frame)
 
-    async def movement(self, last_frame_file) -> bool:
-        ret, frame = self.cap.read()
-        if not ret:
-            raise ValueError('Failed to read frame')
-        last_frame = imread(last_frame_file)
-        if last_frame is None:
-            raise ValueError('Failed to read last frame')
-        diff = self.compare_frames(frame, last_frame)
-        print(f'Difference: {diff}')
-        return diff < 0.1
+    async def movement(self) -> bool:
+        for i, cap in enumerate(self.caps):
+            last_frame_file = f'last_frame_{i}.png'
+            ret, frame = cap.read()
+            if not ret:
+                return False
+            last_frame = imread(last_frame_file)
+            if last_frame is None:
+                return True
+            diff = self.compare_frames(frame, last_frame)
+            print(f'Difference {i}: {diff}')
+            if diff < 0.1:
+                return True
+        return False
 
     @staticmethod
     def compare_frames(frame1, frame2) -> float:
@@ -39,8 +46,10 @@ class Bot(discord.Client):
     async def on_ready(self):
         if not self.user_id:
             raise ValueError('User ID is required')
-        self.cap = VideoCapture(0)
-        if self.cap.isOpened():
+        caps = [VideoCapture(i) for i in range(self.max_cams)]
+        await asyncio.sleep(1)
+        self.caps = [cap for cap in caps if cap.isOpened()]
+        if not self.caps:
             print('No webcam found')
         self.chaoslynx_user = await self.fetch_user(self.user_id)
         if self.chaoslynx_user is None:
@@ -48,8 +57,11 @@ class Bot(discord.Client):
         self.monitor.start()
         print('Bot is ready')
 
-    async def send_image_to_discord(self, file_path):
-        await self.chaoslynx_user.send(file=discord.File(file_path))
+    async def send_images_to_discord(self):
+        assert isinstance(self.caps, list)
+        for i in range(len(self.caps)):
+            file_path = f'last_frame_{i}.png'
+            await self.chaoslynx_user.send(file=discord.File(file_path))
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -57,11 +69,12 @@ class Bot(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
         if message.content == 'show':
-            await self.send_image_to_discord('last_frame.png')
+            await self.send_images_to_discord()
 
 def main(**kwargs):
     token = kwargs.get('token')
     user_id = kwargs.get('user_id')
+    max_cams = int(kwargs.get('max_cams', 1))
     intents = discord.Intents.default()
     intents.dm_messages = True
     intents.message_content = True
@@ -70,6 +83,7 @@ def main(**kwargs):
 
     discord_client = Bot(intents=intents)
     discord_client.user_id = user_id
+    discord_client.max_cams = max_cams
     discord_client.run(token)
 
 if __name__ == '__main__':
